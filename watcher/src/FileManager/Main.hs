92303872ad5b41cccf66e63e9635e5ef63de93f4
Pavel Ponomarev <pavponn@yandex.ru>
2020-04-29 23:24:31 +0300

    Add cat command

--- watcher/src/FileManager/Main.hs
+++ watcher/src/FileManager/Main.hs
@@ -24,32 +24,41 @@ data FSException
   | PermissionDenied
   deriving (Show)
 
+---TODO solve ByteString vs String problem
 main :: IO ()
 main = do
   fs <- getCurrentDirectory >>= \curDir -> getFileSystem curDir
   let state = MyState fs $ getRootDirectory fs
   s <- getLine
-  let (res, st) = runState (runExceptT $ information s) state
+  let (res, st) = runState (runExceptT $ fileContent s) state
   case res of
     Left err      -> putStrLn $ show (err :: FSException)
-    Right content -> putStrLn content
+    Right content -> B.putStrLn $ content
   return ()
 
+fileContent :: FilePath -> ExceptT FSException (State FSState) B.ByteString
+fileContent path = runFunctionWithPath getFileContent path
+  where
+    getFileContent :: Directory -> [FilePath]
+                   -> ExceptT FSException (State FSState) B.ByteString
+    getFileContent dir []     = throwE NoSuchFileOrDirectory
+    getFileContent dir (x:xs) = do
+      fileOrDir <- isInDirectory dir x
+      case fileOrDir of
+        (Left file) ->
+          if (xs == []) then return $ getFileData file
+          else throwE NotDirectory
+        (Right dir') ->
+          if (xs == []) then throwE NoSuchFileOrDirectory
+          else getFileContent dir' xs
+
 information :: FilePath -> ExceptT FSException (State FSState) String
-information path = do
-  if (isAbsolute path) then do
-    MyState{fileSystem = fs} <- get
-    let dirs = tail (splitDirectories path)
-    getInformation (getRootDirectory fs) dirs
-  else do
-    MyState{curDirectory = dir} <- get
-    let dirs = splitDirectories path
-    getInformation dir dirs
+information path = runFunctionWithPath getInformation path
   where
     getInformation :: Directory -> [FilePath]
                    -> ExceptT FSException (State FSState) String
-    getInformation dir []       = return $ show $ getDirInfo dir
-    getInformation dir (x : xs) = do
+    getInformation dir []     = return $ show $ getDirInfo dir
+    getInformation dir (x:xs) = do
       a <- isInDirectory dir x
       case a of
         (Left file)  -> do
@@ -64,20 +73,13 @@ information path = do
             getInformation dir' xs
 
 directoryContent :: FilePath -> ExceptT FSException (State FSState) String
-directoryContent path = do
-  if (isAbsolute path) then do
-    MyState{fileSystem = fs} <- get
-    let dirs = tail (splitDirectories path)
-    showContentInDirectory (getRootDirectory fs) dirs
-  else do
-    MyState{curDirectory = dir} <- get
-    showContentInDirectory dir (splitDirectories path)
+directoryContent path = runFunctionWithPath showContentInDirectory path
   where
     showContentInDirectory :: Directory -> [FilePath]
                            -> ExceptT FSException (State FSState) String
     showContentInDirectory dir [] =
       return $ intercalate ("\n") $ map fst $ Map.toList $ getDirContents dir
-    showContentInDirectory dir (x : xs) = do
+    showContentInDirectory dir (x:xs) = do
       fileOrDir <- isInDirectory dir x
       case fileOrDir of
         (Left _)     -> throwE NotDirectory
@@ -88,3 +90,17 @@ isInDirectory dir name =
   case Map.lookup name (getDirContents dir) of
     Nothing  -> throwE NoSuchFileOrDirectory
     (Just a) -> return a
+
+type ExceptState a = ExceptT FSException (State FSState) a
+
+runFunctionWithPath :: (Directory -> [FilePath] -> (ExceptState a)) -> FilePath
+                    -> (ExceptState a)
+runFunctionWithPath foo path = do
+  if (isAbsolute path) then do
+    MyState{fileSystem = fs} <- get
+    let dirs = tail $ splitDirectories path
+    foo (getRootDirectory fs) dirs
+  else do
+    MyState{curDirectory = dir} <- get
+    let dirs = splitDirectories path
+    foo dir dirs
