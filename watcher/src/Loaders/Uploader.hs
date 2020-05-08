4ddb68b8edeea0e8f0156c2d9316d96f7fa1a51f
Pavel Ponomarev <pavponn@yandex.ru>
2020-05-08 12:48:12 +0300

    Upload VCS to system

--- watcher/src/Loaders/Uploader.hs
+++ watcher/src/Loaders/Uploader.hs
@@ -2,9 +2,10 @@
   ( uploadFileSystem
   ) where
 
-import Control.Exception (SomeException, catch)
+import Control.Exception (SomeException, catch, throw)
 import Control.Monad
 import qualified Data.ByteString as B
+import qualified Data.ByteString.UTF8 as BU
 import Data.Either (partitionEithers)
 import qualified Data.Map.Strict as Map
 import FileManager.FileSystemTypes
@@ -12,6 +13,7 @@ import System.Directory (createDirectoryIfMissing, listDirectory, removeDirector
                          removeFile, writable)
 import System.FilePath.Posix (takeDirectory, (</>))
 import Utils.LoaderUtils
+import System.FilePath.Posix (dropTrailingPathSeparator)
 
 uploadFileSystem :: FileSystem -> IO ()
 uploadFileSystem fs = do
@@ -32,7 +34,7 @@ uploadDirectory dir = do
   let (filesInDir, subDirs) = (partitionEithers . map snd) $ Map.toList dirContents
   forM_ filesInDir uploadFile
   forM_ subDirs uploadDirectory
-  -- upload VCS
+  uploadVCS path (getVCSStorage dir)
 
 uploadFile :: File -> IO ()
 uploadFile file = do
@@ -44,6 +46,49 @@ uploadFile file = do
   else
     B.writeFile filePath content `catch` nezapisalos file
 
+uploadVCS :: FilePath -> Maybe VCSStorage -> IO ()
+uploadVCS _    Nothing   = return ()
+uploadVCS path (Just st) = do
+  let vcsPath = path </> ".vcs"
+  let vcsFilesData = Map.toList $ getVCSFiles st
+  forM_ vcsFilesData (uploadVCSFile vcsPath) `catch` (vcsExceptionHandler vcsPath 1)
+  isDir <- isDirectory path ".vcs"
+  if isDir then
+    (createIndexFile vcsPath st) `catch` (vcsExceptionHandler vcsPath 2)
+  else
+    return ()
+  where
+    vcsExceptionHandler :: FilePath -> Integer -> SomeException -> IO ()
+    vcsExceptionHandler vcsPath i = \_ -> do
+      putStrLn $ "error while writing VCS, VCS won't be uploaded to system " ++ show i
+      removeDirectorySafe vcsPath
+
+createIndexFile :: FilePath -> VCSStorage -> IO ()
+createIndexFile vcsPath st = do
+  let fileName = "index_" ++ (show $ getRevisionsNum st)
+  let indexesToMessages = Map.toList $ Map.unions $ Map.elems $ getVCSFiles st
+  let content = B.concat $ map getLineForRev indexesToMessages
+  createDirectoryIfMissing True vcsPath
+  B.writeFile (vcsPath </> fileName) content `catch` rethrowHandler
+  where
+    getLineForRev = \(index, (_, msg)) -> BU.fromString $ (show index) ++ "->" ++ msg ++ "\n"
+
+uploadVCSFile :: FilePath -> (FilePath, (Map.Map Integer (File, String))) -> IO ()
+uploadVCSFile vcsPath (pathToFile, revisions) = do
+  let pathToFileDir = (dropTrailingPathSeparator vcsPath) ++ pathToFile ++ "/"
+  let listOfRevs = Map.toList revisions
+  forM_ listOfRevs (writeRev pathToFileDir) `catch` rethrowHandler
+  where
+    writeRev :: FilePath -> (Integer, (File, String)) -> IO ()
+    writeRev pathToFileDir = \(index, (file, _)) ->
+      writeToFileWithName pathToFileDir (show index) (getFileData file)
+
+writeToFileWithName :: FilePath -> FilePath -> B.ByteString -> IO ()
+writeToFileWithName dirPath fileName content = do
+  let realPath = dirPath </> fileName
+  createDirectoryIfMissing True $ takeDirectory dirPath
+  B.writeFile realPath content `catch` rethrowHandler
+
 removeDirectorySafe :: FilePath -> IO ()
 removeDirectorySafe path = do
   removeDirectoryRecursive path `catch` (removeDirExceptionHandler path)
@@ -51,6 +96,9 @@ removeDirectorySafe path = do
 removeFileSafe :: FilePath -> IO ()
 removeFileSafe path = do
   removeFile path `catch` (removeFileExceptionHandler path)
+
+rethrowHandler :: SomeException -> IO ()
+rethrowHandler  = \ex -> throw ex
 
 nezapisalos :: File -> SomeException -> IO ()
 nezapisalos file = \_ -> putStrLn $ "ne zapisal :/\n" ++ (show file)
